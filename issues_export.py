330541f516d7549d19189d91d36dd62cfe4fcbe3
shreyas pandya <pandyashreyas1@gmail.com>
2013-09-08 10:10:32 +0530

    script just works to export issues as json

--- issues_export.py
+++ issues_export.py
@@ -9,23 +9,32 @@ class Issue(object):
         self.accountname = accountname
         self.repo_slug = repo_slug
         self.password = password
-        self.endpoint = self.endpoint_schema.format({'accountname':accountname, 'repo_slug':repo_slug})
+        self.endpoint = self.endpoint_schema.format(**{'accountname': accountname, 'repo_slug': repo_slug})
 
     def fetch(self):
         resp = requests.get(self.endpoint, auth=(self.accountname, self.password))
-        return resp.json()
+        return resp.json()['issues']
 
 
 class Comment(object):
     endpoint_schema = 'https://bitbucket.org/api/1.0/repositories/{accountname}/{repo_slug}/issues/{issue_id}/comments'
 
+    def __init__(self, issue_obj):
+        self.issue = issue_obj
+
+    def fetch(self, issue_id):
+        self.endpoint = self.endpoint_schema.format(**{'accountname': self.issue.accountname, 'repo_slug': self.issue.repo_slug,
+                                                    'issue_id': issue_id})
+        resp = requests.get(self.endpoint, auth=(self.issue.accountname, self.issue.password))
+        return resp.json()
+
 
 def export_all(accountname, repo_slug, password):
     issue_obj = Issue(accountname=accountname, repo_slug=repo_slug, password=password)
     issues = issue_obj.fetch()
+    cmnt_obj = Comment(issue_obj)
     for issue in issues:
-        cmnt_obj = Comment(issue)
-        issue['comments'] = cmnt_obj.fetch()
+        issue['comments'] = cmnt_obj.fetch(issue['local_id'])
     return json.dumps(issues)
 
 
